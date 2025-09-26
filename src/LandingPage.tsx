a7af6b85135e8fa42d194170110b099262eb77ed
Gregory Bolkenstijn <greg@soundofsilver.nl>
2025-09-26 13:37:40 +0200

    Creates React app

---
+++ src/LandingPage.tsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export function LandingPage() {
+  return (
+    <div className="bg-[#cc5500] h-dvh m-0 flex items-center justify-center font-serif">
+      <h1 className="text-[#fffdd0] text-6xl font-bold m-0">Sllow</h1>
+    </div>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+
+import { LandingPage } from './LandingPage.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <LandingPage />
+  </StrictMode>
+)
+// test
+// test
